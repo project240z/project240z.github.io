ab87191016f23a18807fd70d80735a7bde1fe002
ChrisHammond <ChrisHammond@users.noreply.github.com>
2020-11-24 06:14:32 +0000

    deploy: 138914c7a2efc8bf975d86ba52b3b5738deac198

---
+++ assets/js/lunrsearchengine.js
@@ -0,0 +1,129 @@
+
+var documents = [{
+    "id": 0,
+    "url": "/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "/about",
+    "title": "About Project240z.com",
+    "body": " A 2016 Scion FR-S campaigned by Chris Hammond We’ve had a variety of success with the car. Here are some results. 2020: 1st Place St. Louis Region SCCA Index Championship (Hammond) 2019: 2nd Place St. Louis Region SCCA Index Championship (Hammond) 2018: April 15, 2018 - St. Louis Region SCCA Event 1  - 1st in SSCL, 41st on Index (Hammond)  - 1st in SSC, 5th on Index, 7th overall raw time (Hammond)April 22, 2018, Misfit Toys Autocross  - 1st overall raw time (Hammond)April 28/29 2018, Blytheville ProSolo  - 1st in SSC (K. Dietz)  - 3rd in SSC (Hammond)  - 4th in L4 (L. Gill)  - Super Challenge Winner (Dietz)May 20, 2018, St. Louis Region SCCA Event 2  - 1st in SSC (Hammond)  - 2nd in SSC (Sotiropoulis)May 24/25 2018, Lincoln Spring Nationals ProSolo  - 2nd in SSC (Hammond)  - 7th in SSC (Tharpe)May 26/27 2018, Lincoln Spring Nationals Champ Tour  - 1st in SSC (Sotiropoulus)  - 3rd in SSC (Hammond)July 8, 2018 Gateway Autocross Event 3  - 1st in SSC (Sotiropoulus)  - 2nd in SSC (Hammond)July 8, 2018 Gateway Autocross Event 4  - 1st in SSC (Hammond)July 22, 2018 Gateway Autocross Event 5  - 1st in SSC (Sotiropoulus)  - 3rd in SSC (Hammond)July 22, 2018 Gateway Autocross Event 6  - 1st in SSC (Hammond)September 2, 2018 2018 Pro Solo Finale  - 3rd in L4 (Alexandrova)  - 14th in SSC (Hammond)September 7th, 2018 2018 TireRack Solo National Championships  - 7th in SSC (Hammond)  - 14th in SSC (Sotiropoulos)October 28th, 2018 St Louis Solo Event 10  - 1st in SSC (Hammond)  - 3rd in SSC (Sotiropoulos) "
+    }, {
+    "id": 2,
+    "url": "/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "/event-calendar",
+    "title": "BicycleTips.com event-calendar",
+    "body": "Here you will find a list of events that BicycleTips. com thinks you might be interested in! This is not a comprehensive list of all events across the country, or even events in the Mid West, but it does contain events that we think our readers, and our writers, will likely want to take part in. We’ve tried to create a list of the big events, and calendar entries for the various deadlines for those events, to make it helpful to try to get yourselves registered for events that might sell out. Google Calendar Direct Link Want to get something on the list? Drop us a line "
+    }, {
+    "id": 4,
+    "url": "/",
+    "title": "Home",
+    "body": "      Featured:                                                                                                                                                                                                             2020 Topeka ProSolo Results                              :               Well that was a productive weekend! I left for Topeka Kansas on Friday morning after working for a few hours and stopped on the drive. . . :                                                                       01 Jul 2020                                                                                                                      All Stories:                                                                               2020 Topeka ProSolo Results              :       Well that was a productive weekend! I left for Topeka Kansas on Friday morning after working for a few hours and stopped on the drive out for a few more. . . :                               01 Jul 2020                                            "
+    }, {
+    "id": 5,
+    "url": "/blog/2020-topeka-prosolo-results",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 6,
+    "url": "/Blog/2020-topeka-prosolo-results",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 7,
+    "url": "/About",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 8,
+    "url": "/Results",
+    "title": "",
+    "body": ""
+    }, {
+    "id": 9,
+    "url": "/redirects.json",
+    "title": "",
+    "body": "{“/blog/2020-topeka-prosolo-results”:”/2020-topeka-prosolo-results”,”/Blog/2020-topeka-prosolo-results”:”/2020-topeka-prosolo-results”,”/About”:”/about”,”/Results”:”/about”} "
+    }, {
+    "id": 10,
+    "url": "/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 11,
+    "url": "/2020-topeka-prosolo-results",
+    "title": "2020 Topeka ProSolo Results",
+    "body": "2020/07/01 - Well that was a productive weekend! I left for Topeka Kansas on Friday morning after working for a few hours and stopped on the drive out for a few more phone calls that I needed to have. I stopped and went into two locations on the drive down, a QuikTrip in Wentzville Missouri, and a Chick-Fil-A in Blue Springs. The QT had no workers or customers (besides myself) wearing a mask. The Chick-Fil-A was still closed for indoor service and all staff were wearing masks while working the drive through lines. I was able to get out to Topeka in time get the Falken RT660s mounted on the car and to get some practice starts in with the new starting lights that they have going this year. My first impression of the lights, they change everything. In the olden days for ProSolo, back before LED lights, I used to leave when the second light was &ldquo;going off&rdquo;. With the LED lights introduced in&nbsp; the past couple of years, I would leave when the third light was coming on. With the 2020 version of the lights, I had to wait until the third light was on and then start the process of releasing the clutch to get the car to move. My first pass at the lights I turned a . 391 reaction time, where a . 500 is &ldquo;prefect, and anything less than . 500 is RED. &nbsp; My second attempt resulted in a . 744, not red, but definitely not in the . 5-600 range that I expect when I&rsquo;m starting. My third attempt was a . 575 much more respectable. I ended up taking 8 practice starts, and while I wasn&rsquo;t really happy with them, I felt at least comfortable enough for the weekend that I didn&rsquo;t buy more. Friday night I took the car by the car wash, grabbed a Vegan Lifestyle bowl from Chipotle and ran around to a couple of liquor stores in Topeka looking to add to my whiskey/bourbon collection. Friday night I spent my first night in a hotel with social distancing and cleaning in place. The Hampton Inn had the rooms &ldquo;tapes&rdquo; off with a seal that said they were clean, if the seal was broken you knew it hadn&rsquo;t been cleaned. The elevator had a sign outside of it requesting that only 2 people use an elevator at a time. Overall the hotel experience was great, except for the fact that it was completely full with pre-teens running around on Saturday evening, obviously a sports tournament of some sort brought a lot of kids in, without any masks to be found.  Saturday morning I walked out of the hotel with the front desk person saying &ldquo;stay dry&rdquo;. I thought, oh it is just a little rain, then the doors parted and I saw that it was a torrential downpour. I navigated out to Heartland Park, going about 45 on the highway due to the conditions and the RT660s feeling a little skatier than I would have liked on the highway. Getting out to the event site, I was my usual self, the first person out walking the course. By the time I arrived, it was still raining and windy, but wasn&rsquo;t the downpour from the hotel so I walked in just the rain jacket and flip flops for shoes. Went through one area where the standing/running water was a good 2&rdquo; deep, but overall managed to not get myself too soaked. There was plenty of wind damage around the site however.   Saturday morning&rsquo;s start was delayed due to lightning (as was Sundays) but by the time my group ran, it was pretty much dry, with conditions getting better as the morning went on. After the Saturday morning sessions I was sitting in 2nd behind Kinch out of Colorado. Saturday afternoon brought more of the same. I felt I was struggled on the right side course, where I felt a lot more comfortable on the left. Saturday afternoon I found . 15 seconds on the left, and just over . 3 on the right Saturday afternoon. My first run on Sunday morning I started on the right and couldn&rsquo;t find any time, but found about . 17 on the left, putting me within striking distance of Kinch. My second right side run I hoped I would find another couple of tenths but struggled and was . 4 seconds slower than my best attempt on Saturday afternoon. Going into the last right on the left side I was pretty sure that I had no chance, and was going to have to settle for second place behind Kinch. I was pleasantly surprised when I came across on my last run with another . 13 second improvement, enough to give me a . 052 win over Kinch. This was my first ProSolo win. The car has won previously, multiple times, but for me, in twenty years of autocrossing, this was a first, and it was a blast! Winning your class in a Pro means you move on to the Challenge the second half of Sunday.  This was I believe my fourth attempt at a challenge, and likely the first time I&rsquo;ve moved on passed the first round. I ended up paired up with James Harnish out of Arizona in the first round. I didn&rsquo;t feel too great about my runs, but he ended up hitting a cone and that gave me the padding I needed to move on to the next round. Round number 2 I was lined up against Craig Wilcox. I&rsquo;ve known Craig since 2003 or 2004 when we both raced 350Zs in B-Stock. He&rsquo;s out of KC and has multiple national championships under his belt, while I, have none. Sitting at the back of the line waiting for the round to proceed Craig started running around debating on whether or not he needed to get gas for his SMF Honda, I selfishly started wondering if that might not play into my own advantage. It didn&rsquo;t. If anything it took me out of my game. I realized just before pulling to the line that I hadn&rsquo;t set my tire pressures. After round one I had gotten out and check them, but decided to set them closer to running. By the time I realized I didn&rsquo;t actually set them, it was too late, I was strapped in the car and they were getting ready to send us to the line. I tried to put that out of my head and just head on out. My first run was once again on the Right side course. I fell asleep at the lights and left with a reaction time of . 993 almost half a second off of the ideal reaction time, and three tenths greater than what I would consider acceptable. I didn&rsquo;t feel great on the run, but it is also the slower side that I struggled with all weekend, I came back with a 1. 001 deficit, almost . 5 on the light, and another . 6 on the slower course, I felt like that math worked. Going into the left side run I knew I was going to need to push it, and Craig might back off knowing he didn&rsquo;t quite need to push as hard. I left on the last run with a . 623 reaction time and pushed as hard as I could to try to find time. I looked across to the other course as I came through the finish and I was quite a bit farther than Craig, I hoped deep down inside that it was enough. Once Craig crossed the line the announcer stated that I had made up over 8 tenths, but not the full second I needed, so I was done and Craig was moving on to the next round against Kinch. Here&rsquo;s the video of my best left and right side runs (Sunday morning and Saturday afternoon respectively). The left side run uses my GoPro Hero8 with stability enabled, on a fixed mount on the roof of the car. The right side run uses the same camera with stability, but on my rotating mount. I honestly don&rsquo;t feel like there is a big difference between the two mounts with the &ldquo;hypersmooth&rdquo; enabled in the GoPro Hero8.  My drive home on Sunday was uneventful, changed tires, loaded up the car, and hit the road. I averaged around 32mpg between Topeka and St. Louis, getting home just after dark so that I could unload the car and see the family before everyone fell asleep. Check out additional photos from the weekend.  Autocross Settings:: Front Swaybar:&nbsp;Soft settingRear Swaybar:&nbsp;Stiff SettingFront Konis:&nbsp;0. 5 turn from softRear Konis:&nbsp;1. 5 turns from softFront Tire Pressure:&nbsp;30&nbsp;psiRear Tire Pressure:&nbsp;29&nbsp;psi "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
